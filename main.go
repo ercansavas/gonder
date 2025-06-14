b07b4f1c5506110755e1f273a62843ec02f26bb3
ercansavas <ercansavas1@gmail.com>
2025-06-15 01:09:12 +0300

    Fix formatting in main.go to ensure consistent indentation and add newline at end of file

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Merhaba! Go başarıyla kuruldu! 🎉")
-    fmt.Println("Hello! Go has been successfully installed! 🎉")
-} 
+	fmt.Println("Merhaba! Go başarıyla kuruldu! 🎉")
+	fmt.Println("Hello! Go has been successfully installed! 🎉")
+}
